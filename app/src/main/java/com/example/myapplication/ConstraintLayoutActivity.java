c8fd0b45adda3ddd76282182d94d751a7e74d936
Admin <surakshamysore17@gmail.com>
2020-12-24 16:48:59 +0530

    initial comit

--- app/src/main/java/com/example/myapplication/ConstraintLayoutActivity.java
+++ app/src/main/java/com/example/myapplication/ConstraintLayoutActivity.java
@@ -1,14 +1,38 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConstraintLayoutActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint_layout);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationIcon(R.drawable.ic_keyboard_arrow_left_black_24dp);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        button = (Button)findViewById(R.id.buttonsig);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ConstraintLayoutActivity.this,SignupActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
