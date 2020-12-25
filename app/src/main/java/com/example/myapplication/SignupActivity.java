82169fdfc517a447eb58e928335e2dba128bbe41
Admin <surakshamysore17@gmail.com>
2020-12-25 15:51:28 +0530

    initial comit

--- app/src/main/java/com/example/myapplication/SignupActivity.java
+++ app/src/main/java/com/example/myapplication/SignupActivity.java
@@ -11,4 +11,9 @@ public class SignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
     }
+
+    public void hello()
+    {
+        System.out.println("Hello");
+    }
 }
