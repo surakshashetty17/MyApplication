44fc29f09d75c9ae1daa7f012a72605f4c4d569b
Admin <surakshamysore17@gmail.com>
2021-01-05 17:04:12 +0530

    initial comit

--- app/src/main/java/com/example/myapplication/BusinessDetailsActivity.java
+++ app/src/main/java/com/example/myapplication/BusinessDetailsActivity.java
@@ -12,10 +12,15 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BusinessDetailsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     CheckBox checkBox1,checkBox2,checkBox3,checkBox4,checkBox5,checkBox6,checkBox7;
     Button save_changes;
+    String[] interest;
+    ArrayList<String> interestList;
     String[] start_time = { "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM", "12AM"};
     String[] stop_time = { "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM", "12AM"};
     @Override
@@ -41,24 +46,66 @@ public class BusinessDetailsActivity extends AppCompatActivity implements Adapte
         checkBox6=(CheckBox)findViewById(R.id.checkBox6);
         checkBox7=(CheckBox)findViewById(R.id.checkBox7);
 
-        Spinner spin = (Spinner) findViewById(R.id.spinner1);
-        spin.setOnItemSelectedListener(this);
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,start_time);
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Setting the ArrayAdapter data on the Spinner
-        spin.setAdapter(aa);
-
-        Spinner spin1 = (Spinner) findViewById(R.id.spinner2);
+        Spinner spin1 = (Spinner) findViewById(R.id.spinner1);
         spin1.setOnItemSelectedListener(this);
-        ArrayAdapter aa1 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,stop_time);
+        ArrayAdapter aa1 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,start_time);
         aa1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Setting the ArrayAdapter data on the Spinner
         spin1.setAdapter(aa1);
+
+        Spinner spin2 = (Spinner) findViewById(R.id.spinner2);
+        spin2.setOnItemSelectedListener(this);
+        ArrayAdapter aa2 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,stop_time);
+        aa2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin2.setAdapter(aa2);
 
         save_changes = (Button)findViewById(R.id.save_changes);
         save_changes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                interestList = new ArrayList<>();
+
+                // for each checkbox checked, accumulate a category id in the list
+                if (checkBox1.isChecked()) {
+                    interestList.add("catagory_id=1");
+                }
+
+                if (checkBox2.isChecked()) {
+                    interestList.add("catagory_id=2");
+                }
+
+                if (checkBox3.isChecked()) {
+                    interestList.add("catagory_id=3");
+                }
+
+                if (checkBox4.isChecked()) {
+                    interestList.add("catagory_id=4");
+
+                }
+                if (checkBox5.isChecked()) {
+                    interestList.add("catagory_id=5");
+                }
+
+                if (checkBox6.isChecked()) {
+                    interestList.add("catagory_id=6");
+                }
+
+                if (checkBox7.isChecked()) {
+                    interestList.add("catagory_id=7");
+                }
+                // convert the list into an array
+                interest = interestList.toArray(new String[interestList.size()]);
+
+                System.out.println("Selected");
+                for (String str : interest) {
+                    System.out.println(str);
+                }
+
+                System.out.println("List Length"+interest.length);
+                System.out.println("Array Size"+interestList.size());
+
+
                 Intent i = new Intent(BusinessDetailsActivity.this,SpinnerCheckboxActivity.class);
                 startActivity(i);
             }
