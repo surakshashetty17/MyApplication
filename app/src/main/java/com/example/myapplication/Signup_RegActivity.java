2e65af9f33b7f0fe709e62007d0c44140d7b498a
Admin <surakshamysore17@gmail.com>
2021-02-22 10:38:07 +0530

    initial comit

--- app/src/main/java/com/example/myapplication/Signup_RegActivity.java
+++ app/src/main/java/com/example/myapplication/Signup_RegActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,8 +13,22 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.myapplication.Common.Constants;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,6 +37,7 @@ public class Signup_RegActivity extends AppCompatActivity {
     EditText User_name,Email,Mobile,Password,ReenterPass;
     ImageButton img1, img2;  //  password eye
     private int passwordNotVisible = 1;
+    String sign_url = "http://192.168.0.147/mobile-api/index.php/api/signup";
     Button register;
     TextView login;
 
@@ -39,7 +55,6 @@ public class Signup_RegActivity extends AppCompatActivity {
         img2 = (ImageButton) findViewById(R.id.image_repass);
         login = (TextView) findViewById(R.id.signup_reglog);
         register = (Button) findViewById(R.id.signup_regNow);
-
 
 
         //show/hide password
@@ -111,12 +126,12 @@ public class Signup_RegActivity extends AppCompatActivity {
                     } else {
 //                        if (Constants.isOnline(getApplicationContext())) {
 ////                            //String SIGNUP_URL = CommonUtils.baseurl + "api/user/sign_up";
-////                            register(userName, userMobile, userEmail, userPassword, userRePassword);
+                            register(userName, userMobile, userEmail, userPassword, userRePassword);
 ////                        } else {
 //                            Toast.makeText(getApplicationContext(),"connection failed",Toast.LENGTH_LONG).show();
 //                        }
-                        Intent i = new Intent(Signup_RegActivity.this,SignInActivity.class);
-                        startActivity(i);
+//                        Intent i = new Intent(Signup_RegActivity.this,SignInActivity.class);
+//                        startActivity(i);
                     }
                 }
                 else {
@@ -134,7 +149,107 @@ public class Signup_RegActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-
-
     }
+
+    private void register(final String getuserName, final String getuserMobile,
+                          final String getuserEmail, final String getuserPassword, final String getReenterPass){
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://192.168.0.147/mobile-api/index.php/api/signup",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj = new JSONObject(response);
+
+                            Toast.makeText(getApplicationContext(), obj.getString("message"),Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }){
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params  = new HashMap<>();
+                params.put("username",getuserName);
+                params.put("mobile", getuserMobile);
+                params.put("email",getuserEmail);
+                params.put("password", getuserPassword);
+                params.put("cpassword",getReenterPass);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+
+//        URL url = new URL("http://192.168.0.147/mobile-api/index.php/api/signup");
+//        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//        con.setRequestMethod("POST");
+        //if everything is fine
+//        StringRequest stringRequest = new StringRequest(Request.Method.POST,"http://192.168.0.147/mobile-api/index.php/api/signup",
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        //   progressBar.setVisibility(View.GONE);
+//
+////                        Log.e("Response"+response);
+//                        Log.e("hello", "Response from url: " + response);
+//
+//                        try {
+//                            //converting response to json object
+//                            JSONObject obj = new JSONObject(response);
+//                            int error = obj.getInt("error");
+//                            //if no error in response
+//                            if(error==1) {
+//
+//                                    Toast.makeText(getApplicationContext(), "Successfully Signed In", Toast.LENGTH_SHORT).show();
+////                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+//
+//
+////                            startActivity(new Intent(Signup_RegActivity.this, SignInActivity.class));
+//                                Intent i = new Intent(Signup_RegActivity.this,SignInActivity.class);
+//                                startActivity(i);
+////                            finish();
+//                            } else {
+//                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+//                            }
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                }) {
+//            @Override
+//            protected Map<String, String> getParams() throws AuthFailureError {
+//                Map<String, String> params = new HashMap<>();
+//                params.put("username",getuserName);
+//                params.put("mobile", getuserMobile);
+//                params.put("email",getuserEmail);
+//                params.put("password", getuserPassword);
+//                params.put("cpassword",getReenterPass);
+//                return params;
+//            }
+//        };
+//
+////        VolleySingleton.getInstance(this).addToRequestQueue(stringRequest);
+//        RequestQueue rq= Volley.newRequestQueue(this);
+//        rq.add(stringRequest);
+//    }
+
 }
